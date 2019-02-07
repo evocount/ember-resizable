89549d7106fc5d1b0e30785c1dcc01a51c99a2a4
Till Theato <till.theato@evocount.de>
2019-02-07 15:34:14 +0100

    Use ember-bootstrap for demo.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,12 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     snippetPaths: ['tests/dummy/app/snippets'],
+
+    'ember-bootstrap': {
+      'bootstrapVersion': 4,
+      'importBootstrapFont': false,
+      'importBootstrapCSS': false
+    }
   });
 
   /*
