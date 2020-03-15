4c1d15c5e55fc2fb4e04a25fe0b7ccce4984bd23
Till Theato <till.theato@evocount.de>
2020-03-15 12:00:17 +0100

    ember-cli-release-tag instead of outdated ember-cli-release.

--- config/release.js
+++ config/release.js
@@ -1,21 +1,5 @@
-/* jshint node:true */
-// var RSVP = require('rsvp');
+'use strict';
 
-// For details on each option run `ember help release`
 module.exports = {
-  // local: true,
-  // remote: 'some_remote',
-  // annotation: "Release %@",
-  // message: "Bumped version to %@",
-  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-  // publish: true,
-  // strategy: 'date',
-  // format: 'YYYY-MM-DD',
-  // timezone: 'America/Los_Angeles',
-  //
-  // beforeCommit: function(project, versions) {
-  //   return new RSVP.Promise(function(resolve, reject) {
-  //     // Do custom things here...
-  //   });
-  // }
+  strategy: 'semver' // options: semver, date
 };
